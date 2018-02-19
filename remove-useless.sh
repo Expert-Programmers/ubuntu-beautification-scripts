8d2f4b4a8ed896f1154c1315c0ce10c57aaac3de
ltoddy <1205446803@qq.com>
2018-02-19 13:30:49 +0800

    add remove useless softwares script

---
+++ remove-useless.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+useless_softwares=(
+    libreoffice-common
+    unity-webapps-common
+    thunderbird
+    totem
+    rhythmbox
+    simple-scan
+    brasero
+    gnome-mahjongg
+    aisleriot
+    gnome-mines
+    gnome-sudoku
+    onboard
+)
+
+function remove-software() {
+    for useless_software in ${useless_softwares[@]}; do
+        echo "正在移除的软件是: ${useless_software}"
+        sudo apt-get remove $useless_software >& /dev/null
+    done
+}
+
+function main() {
+    echo "将要被移除的软件有:\n\t${useless_softwares[@]}"
+    while [ true ]; do
+        read -p "是否要继续? (yes/no) > " choose
+        case $choose in
+            yes)
+                remove-software
+                break
+            ;;
+            no)
+                exit 0
+            ;;
+            *)
+                echo "输入有误,请重新输入."
+        esac
+    done
+    echo "全部移除完毕."
+}
+
+main
